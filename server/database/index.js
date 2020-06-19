711e3371fc35cb98c298d34cf9789d658f35972c
Jing Chen <jchen0506@gmail.com>
2020-06-18 21:08:43 -0500

    Created random data generator and inserted 150 campsite data point into DB

--- server/database/index.js
+++ server/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const mongoUri = 'mongodb://localhost/photos';
+const mongoUri = 'mongodb://localhost/campsite';
 
-mongoose.connect(mongoUri, { useNewUrlParser: true });
+mongoose.connect(mongoUri, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
