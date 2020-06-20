08aaec4a330fe49ba40e884ffbad9a935d2ab1d5
Jing Chen <jchen0506@gmail.com>
2020-06-19 19:37:20 -0500

    Setup jest for mongo and wrote first test

--- _test_/server.test.js
+++ _test_/server.test.js
@@ -1,11 +1,45 @@
 const request = require('supertest');
 const app = require('../server/app');
+const mongoose = require('mongoose');
+const Camp = require('../server/database/Camp.js');
 
-// describe('GET /:campid ', () => {
-//   test('It should respond with the campsite json object accordingly to the id', async () => {
-//     const response = await request(app).get('/1');
-//     expect(response.body.location).toEqual('Mann Trail');
-//     expect(response.body.photos.length).toEqual(6);
-//     expect(response.statusCode).toBe(200);
-//   });
-// });
+describe('server test ', () => {
+  beforeAll(async () => {
+    const url = `mongodb://127.0.0.1/testDB`;
+    await mongoose.connect(url, { useNewUrlParser: true });
+  });
+
+  afterEach(async () => {
+    await Camp.deleteMany();
+  });
+
+  it('should respond with the campsite json object accordingly to the id', async () => {
+    var testCamp = new Camp({
+      id: '1',
+      location: 'Texas',
+      photos: [
+        {
+          url: 'www.fakeurl.com',
+          user: {
+            username: 'Jing',
+            profile_pic_url: 'www.fakeavatar.com',
+          },
+          description: 'This is a test',
+          thumbs: 5,
+          date: '2020-04-23T07:31:49.717Z',
+        },
+      ],
+    });
+
+    testCamp.save((err, camp) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    const response = await request(app).get('/1');
+    expect(response.body[0].location).toEqual('Texas');
+    expect(response.body[0].photos.length).toEqual(1);
+    expect(response.statusCode).toBe(200);
+  });
+});
