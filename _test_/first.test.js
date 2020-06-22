a11081990511797a1e9cd4d6646c5bab381b6c57
Jing Chen <jchen0506@gmail.com>
2020-06-22 16:44:53 -0500

    Modified the test according to endpoint change

--- _test_/first.test.js
+++ _test_/first.test.js
@@ -5,11 +5,3 @@ const app = require('../server/app');
 test('It adds two numbers', () => {
   expect(1 + 1).toBe(2);
 });
-
-describe('GET /hello ', () => {
-  test('It should respond with hello world', async () => {
-    const response = await request(app).get('/hello');
-    expect(response.body).toEqual('Hello World');
-    expect(response.statusCode).toBe(200);
-  });
-});
