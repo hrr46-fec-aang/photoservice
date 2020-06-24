8f380baf1e26b6ab2eea2346196431c1d5ca6b77
Jing Chen <jchen0506@gmail.com>
2020-06-24 17:36:50 -0500

    Config jest with enzyme and babele

---
+++ _test_/client.test.js
@@ -0,0 +1,11 @@
+import { shallow, mount, render } from 'enzyme';
+
+describe('My Test Suite', () => {
+  it('My Test Case', () => {
+    expect(true).toEqual(true);
+  });
+});
+
+// describe('<App >', () => {
+//   it()
+// })
